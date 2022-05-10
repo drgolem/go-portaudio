5374ba1a0bc0c3f042be026276ca3440912e4af6
Valery Gridnev <valer.gr@gmail.com>
2022-05-10 21:50:48 +1000

    example device enum

---
+++ examples/audio_enum.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DrGolem/go-portaudio/portaudio"
+)
+
+func main() {
+	fmt.Println("portaudio device info")
+
+	fmt.Printf("version text: %s\n", portaudio.GetVersionText())
+
+	err := portaudio.Initialize()
+	if err != nil {
+		fmt.Printf("ERR: %v\n", err)
+		return
+	}
+	defer portaudio.Terminate()
+
+	devCnt, err := portaudio.GetDeviceCount()
+	if err != nil {
+		fmt.Printf("ERR: %v\n", err)
+	} else {
+		fmt.Printf("device count: %d\n", devCnt)
+	}
+
+	for devIdx := 0; devIdx < devCnt; devIdx++ {
+		di, err := portaudio.GetDeviceInfo(devIdx)
+		if err != nil {
+			fmt.Printf("ERR: %v\n", err)
+		} else {
+			fmt.Printf("[%d] device: %#v\n", devIdx, di)
+		}
+	}
+
+	hostApiCnt, err := portaudio.GetHostApiCount()
+	if err != nil {
+		fmt.Printf("ERR: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Host API Info (count: %d)\n", hostApiCnt)
+	for idx := 0; idx < hostApiCnt; idx++ {
+		hi, err := portaudio.GetHostApiInfo(idx)
+		if err != nil {
+			fmt.Printf("ERR: %v\n", err)
+		} else {
+			fmt.Printf("[%d] api info: %#v\n", idx, hi)
+		}
+	}
+
+	outStreamParams := portaudio.PaStreamParameters{
+		DeviceIndex:  1,
+		ChannelCount: 2,
+		SampleFormat: portaudio.SampleFmtFloat32,
+		//SampleFormat: portaudio.SampleFmtInt24,
+	}
+	sampleRate := float32(44100)
+	err = portaudio.IsFormatSupported(nil, &outStreamParams, sampleRate)
+	if err != nil {
+		fmt.Printf("ERR: %v\n", err)
+		return
+	} else {
+		fmt.Printf("Format supported: %v, sampleRate: %f\n", outStreamParams, sampleRate)
+	}
+}
